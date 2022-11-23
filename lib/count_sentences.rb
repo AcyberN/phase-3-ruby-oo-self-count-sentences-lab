1f6457a0dd4acce24c57ea96f82d529b5e3c670e
Alexander <newson.alexander@gmail.com>
2022-11-23 11:36:44 -0500

    commit tree b5d51476f81c21a66f358f740325eda94b17b426

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else 
+      false
+    end
   end
 
   def question?
@@ -15,6 +19,9 @@ class String
   end
 
   def count_sentences
-
-  end
+   self.split.count
 end
+
+end
+
+s1=String.new("hey I'm Alex. Youpie!")
